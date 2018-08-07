096311ea9d6917fb0b7175a04d4b608debf9f8c4
Jeremiahsmee <borgerjeremy@gmail.com>
2018-08-07 17:30:34 +0100

    validationVenues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,9 @@ class Venue < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :bookings
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :email, presence: true
+  validates :email, presence: true
+  validates :name, presence: true
 end
