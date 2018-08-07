a2472c473e8250a6f20669002500c26579ebba13
Emmanuel Mawutor <emawutor@indiana.edu>
2018-08-07 22:19:46 +0100

    added the search bar to home and made a card

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :venues
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-validates :first_name, presence: true
-validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
