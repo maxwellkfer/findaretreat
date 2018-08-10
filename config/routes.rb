9f2612c9383c6c94096a0c5946af451f27c72cce
Lucinda Redfern <lucindaredfern@gmail.com>
2018-08-10 14:31:36 +0100

    PROBLEM SOLVED

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :venues
-
-  resources :bookings do
-    resources :reviews, only: :create
+  resources :venues do
+    resources :bookings
   end
 end
